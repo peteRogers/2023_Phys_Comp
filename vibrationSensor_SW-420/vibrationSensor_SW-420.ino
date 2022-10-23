d6f5cc687df15dbff2caa6b14a67893127e7611d
Peter Rogers <p.rogers@gold.ac.uk>
2022-10-23 22:41:36 +0100

    Update vibrationSensor_SW-420.ino

--- vibrationSensor_SW-420/vibrationSensor_SW-420.ino
+++ vibrationSensor_SW-420/vibrationSensor_SW-420.ino
@@ -3,10 +3,10 @@ int vibr_Pin = 3;
 void setup(){
   pinMode(vibr_Pin, INPUT);
   Serial.begin(115200);
-
 }
+
 void loop(){
-  long measurement =measureVib();
+  long measurement = measureVib();
   if(measurement > 0){
     Serial.println(measurement);
     delay(2);
@@ -14,6 +14,6 @@ void loop(){
 }
 
 long measureVib(){
-  long measurement=pulseIn (vibr_Pin, HIGH);  //wait for the pin to get HIGH and returns measurement
+  long measurement = pulseIn(vibr_Pin, HIGH);  //wait for the pin to get HIGH and returns measurement
   return measurement;
 }
