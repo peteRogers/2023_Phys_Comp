ae752f3c83258758e507129529a23389f6d5dae3
Peter Rogers <p.rogers@gold.ac.uk>
2022-10-26 17:20:49 +0100

    Add files via upload

---
+++ neoPixelJewel/jewel_BASIC_Arduino/jewel_BASIC_Arduino.ino
@@ -0,0 +1,14 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 6
+int d = 20;
+Adafruit_NeoPixel jewel = Adafruit_NeoPixel(7, PIN, NEO_GRBW + NEO_KHZ800);
+
+void setup() {
+  jewel.begin();
+}
+
+void loop() {
+  jewel.setPixelColor(0, 0, 0, 0, 255);
+  jewel.show();
+}
